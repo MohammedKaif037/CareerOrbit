1f04ca5d6d48eb2f9ec42925cdfbb24e7fcdac84
Mohammed Kaif <105605883+MohammedKaif037@users.noreply.github.com>
2025-04-29 02:10:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import ClientLayout from "./ClientLayout"
-import { AuthProvider } from '@/lib/auth-context';
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,12 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <AuthProvider>
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ClientLayout>{children}</ClientLayout>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-    </AuthProvider>
   )
 }
