bdfc840acb42626c7461a4dcab7e90a8790d94f6
Mohammed Kaif <105605883+MohammedKaif037@users.noreply.github.com>
2025-04-28 22:42:13 +0530

    Update page.tsx

--- app/{protected}/dashboard/page.tsx
+++ app/{protected}/dashboard/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+export const dynamic = "force-dynamic";
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { PlusCircle, Briefcase, Calendar, CheckCircle, Table2 } from "lucide-react"
