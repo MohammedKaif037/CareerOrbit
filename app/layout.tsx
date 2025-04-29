7d258845ce4c7f4b217cb4ff9a2b1d1701325d1b
Mohammed Kaif <105605883+MohammedKaif037@users.noreply.github.com>
2025-04-30 01:26:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { AuthProvider } from '@/lib/auth-context';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,11 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <AuthProvider>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
       </body>
+      </AuthProvider>
     </html>
   )
 }
